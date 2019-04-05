cfafe17ca54844b90e1dd700790cf61ace1065a0
alikhsan778 <alikhsan778@gmail.com>
2019-04-05 22:54:14 +0700

    change configure connection string

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "avocode.database.windows.net";
+    $user = "alikhsan778";
+    $pass = "Prthelaw07";
+    $db = "dicoding_azure_db";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
